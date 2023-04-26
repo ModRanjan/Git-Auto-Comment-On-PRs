2567389ddd35cbab3754ffa519c70ef219b12120
ModRanjan <ranjan.m@simformsolutions.com>
2023-04-26 11:25:43 +0530

    added-utils

---
+++ utils/GeneralFunctions/index.js
@@ -0,0 +1,53 @@
+export function pageTitle(newTitle) {
+  return (document.title = newTitle);
+}
+
+export function getSessionStorage(key) {
+  return JSON.parse(window.sessionStorage.getItem(key));
+}
+
+export function setSessionStorage(key, value) {
+  // * STORE walletData INTO SESSION
+  return window.sessionStorage.setItem(key, JSON.stringify(value));
+}
+
+export function setLocalStorage(key, value) {
+  // * STORE INTO LOCALSTORAGE SESSION
+  return window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function timeDifference(time) {
+  const currentTime = new Date().getTime();
+  let timestampDifference = new Date(time - currentTime);
+  let year = timestampDifference.getUTCFullYear() - 1970;
+  let month = timestampDifference.getUTCMonth();
+  let day = timestampDifference.getUTCDate() - 1;
+  let hour = timestampDifference.getUTCHours();
+  const diff = {
+    remainingYear: function () {
+      return year > 0 ? year + ' year ' : '';
+    },
+    remainingMonth: function () {
+      return month > 0 ? month + ' month ' : '';
+    },
+    remainingDays: function () {
+      return day > 0 ? day + ' day ' + hour + ' hr' : '';
+    },
+    remainingHour: function () {
+      if (year <= 0 && month <= 0) {
+        return hour > 0
+          ? hour + ' hr'
+          : Math.ceil(timestampDifference / 1000 / 60) + ' min';
+      }
+    },
+  };
+  let timeDifference =
+    diff.remainingYear() +
+    '' +
+    diff.remainingMonth() +
+    '' +
+    diff.remainingDays();
+  timeDifference =
+    timeDifference.length > 0 ? timeDifference : diff.remainingHour();
+  return timeDifference;
+}
---
+++ utils/config/SidebarNavigation.ts
@@ -0,0 +1,38 @@
+import {
+  DashboardIcon,
+  FolderIcon,
+  CalenderIcon,
+  PeoplesIcon,
+  AnalyticsIcon,
+  SettingIcon,
+} from '@/Atoms/Icon/CustomIcons';
+import { SidebarNavItemType } from '@/types/SidebarNavigation';
+
+export const SidebarNavigation: SidebarNavItemType[] = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: DashboardIcon,
+    current: true,
+  },
+  {
+    name: 'Repos',
+    href: '/repositories',
+    icon: FolderIcon,
+    current: false,
+  },
+  { name: 'Prs', href: '/prs', icon: CalenderIcon, current: false },
+  { name: 'AI Guide', href: '/aiGuide', icon: PeoplesIcon, current: false },
+  {
+    name: 'Analytics',
+    href: '/analytics',
+    icon: AnalyticsIcon,
+    current: false,
+  },
+  {
+    name: 'Settings',
+    href: '/setting',
+    icon: SettingIcon,
+    current: false,
+  },
+];
